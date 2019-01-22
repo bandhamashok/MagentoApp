baedca28082dc17f124b94e19f0fcbc10a17ca42
DELL <DELL@DESKTOP-HP81MQU>
2019-01-22 15:27:14 +0530

    second PUSH

--- MavenProject/src/test/java/MagentoTest.java
+++ MavenProject/src/test/java/MagentoTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class MagentoTest
@@ -13,7 +14,7 @@ public class MagentoTest
 	public void magento()
 	{
 		String url="http://www.magento.com";
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(url);
